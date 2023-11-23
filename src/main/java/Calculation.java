ea0cb16282f5f7c8cc45ceb16ee8024cd45c1324
jrosenau <jrosenau@htl-steyr.ac.at>
2023-11-23 13:09:54 +0100

    initial commit

---
+++ src/main/java/Calculation.java
@@ -0,0 +1,8 @@
+public class Calculation{
+    public int calculateArea(int l, int w){
+        return l*w;
+    }
+    public double calculateCircleCircumference(int r){
+        return Math.PI*(2*r);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        System.out.println(calculation.calculateCircleCircumference(3));
+        System.out.println(calculation.calculateArea(4, 2));
+    }
+}
